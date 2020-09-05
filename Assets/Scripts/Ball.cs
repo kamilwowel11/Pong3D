186f11047fadb29f1fd6c28fa8969bc3e8237922
kamilwowel11 <kamilr1998@o2.pl>
2020-09-05 15:06:51 +0200

    Win ball particles

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -8,8 +8,11 @@ public class Ball : MonoBehaviour
     public Vector3 initialImpact;
     public Rigidbody rb;
     public ScoreUI UI;
-    public ParticleSystem boomRed;
-    public ParticleSystem boomBlue;
+    public ParticleSystem boomRedParticles;
+    public ParticleSystem boomBlueParticles;
+    public GameObject Wall1;
+    public GameObject Wall2;
+    private bool color = true;
     Ball ball;
 
 
@@ -18,6 +21,10 @@ public class Ball : MonoBehaviour
     { 
         ball = gameObject.GetComponent<Ball>();
         rb.AddForce(initialImpact, ForceMode.Impulse);
+        
+        //Instantiate(boomRedParticles.gameObject);
+       // Instantiate(boomBlueParticles.gameObject);
+
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -25,20 +32,60 @@ public class Ball : MonoBehaviour
         if (other.tag == "BackPlayerWall")
         {
             UI.scorePlayer++;
+
+            //GameObject vfx;
+            //vfx = Instantiate(boomBlue);
+            //vfx.transform.position = ball.transform.position;
+            //Destroy(vfx, 2f);
+            GameObject vfx;
+            vfx = Instantiate(boomRedParticles.gameObject);
+            vfx.transform.position = ball.transform.position;
+
+            Destroy(vfx, 2f);
+
+
             ball.transform.position = new Vector3(0f, 3.1f, 0f);
             rb.velocity = Vector3.zero;
-            boomBlue.Play();
-
             rb.AddForce(new Vector3(UnityEngine.Random.Range(-10,-11),0f, UnityEngine.Random.Range(-10,-5)), ForceMode.Impulse);
 
         }
         else if(other.tag == "BackEnemyWall")
         {
             UI.scoreEnemy++;
-            boomRed.Play();
+
+            //GameObject vfx;
+            //vfx = Instantiate(boomBlue);
+            //vfx.transform.position = ball.transform.position;
+            //Destroy(vfx, 2f);
+            GameObject vfx;
+            vfx  = Instantiate(boomBlueParticles.gameObject);
+            vfx.transform.position = ball.transform.position;
+
+            Destroy(vfx, 2f);
+
+
+
             ball.transform.position = new Vector3(0f, 3.1f, 0f);
             rb.velocity = Vector3.zero;
             rb.AddForce(new Vector3(UnityEngine.Random.Range(10, 11), 0f, UnityEngine.Random.Range(10, 5)), ForceMode.Impulse);
+        }
+        else if (other.tag == "GameBorder")
+        {
+            var wall1Renderer = Wall1.GetComponent<Renderer>();
+            var wall2Renderer = Wall2.GetComponent<Renderer>();
+            if (color == true)
+            {
+                wall1Renderer.material.SetColor("_Color", Color.red);
+                wall2Renderer.material.SetColor("_Color", Color.red);
+                color = false;
+            }
+            else
+            {
+                wall1Renderer.material.SetColor("_Color", Color.blue);
+                wall2Renderer.material.SetColor("_Color", Color.blue);
+                color = true;
+            }
+            
         }
     }
 
